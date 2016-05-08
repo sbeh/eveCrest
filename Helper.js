6b5500ede0b037fd059f14ae4680d2908d066f47
Stan Behrens <git@sbeh.de>
2016-05-08 16:58:32 +0200

    Beauty FIX: Rename h to href in Helper

--- Helper.js
+++ Helper.js
@@ -17,36 +17,36 @@ var VERBOSE = false
 		return
 
 	// Setup root container for any CREST activity
-	var h = new Href()
+	var href = new Href()
 	//   Set root container as a reference to itself
-	h.root_href = h
+	href.root_href = href
 	//   Setup CREST entry point URL
-	h.href = 'https://crest-tq.eveonline.com/'
+	href.href = 'https://crest-tq.eveonline.com/'
 	//    Start first test request
-	h.get(function () {
+	href.get(function () {
 		// Check if request finished successfully
 		// https://crest-tq.eveonline.com/authEndpoint/ will link to EVE Single sign-on service and is expected to be always there
-		if (!h.authEndpoint) {
-			alert('EVE CREST API: Failed to GET ' + h.href)
+		if (!href.authEndpoint) {
+			alert('EVE CREST API: Failed to GET ' + href.href)
 
 			return
 		}
 
 		// Setup access_token authentication for the CREST URL
-		h.auth = new Auth()
-		h.auth.loc = h
-		h.auth.store = localStorage
+		href.auth = new Auth()
+		href.auth.loc = href
+		href.auth.store = localStorage
 		// Setup refresh_token authentication for the EVE Single sign-on
-		h.auth.request_auth = new Href()
-		h.auth.request_auth.href = h.authEndpoint.href
-		h.auth.request_auth.auth = new Auth()
-		h.auth.request_auth.auth.loc = h.auth.request_auth
-		h.auth.request_auth.auth.type = 'Basic'
-		h.auth.request_auth.auth.token = SECRET
+		href.auth.request_auth = new Href()
+		href.auth.request_auth.href = href.authEndpoint.href
+		href.auth.request_auth.auth = new Auth()
+		href.auth.request_auth.auth.loc = href.auth.request_auth
+		href.auth.request_auth.auth.type = 'Basic'
+		href.auth.request_auth.auth.token = SECRET
 
 		try {
-			h.auth.setup(null, function () {
-				window.eveApi = h
+			href.auth.setup(null, function () {
+				window.eveApi = href
 
 				if (VERBOSE)
 					console.log('EVE CREST API: Ready')
@@ -58,8 +58,8 @@ var VERBOSE = false
 			if (localStorage.state && localStorage.state === params.state) {
 				delete localStorage.state
 
-				h.auth.authorization_code = params.code
-				h.auth.setup(null, function () {
+				href.auth.authorization_code = params.code
+				href.auth.setup(null, function () {
 					location = location.origin + location.pathname
 				})
 			} else {
